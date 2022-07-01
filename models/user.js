d9313ef38f5297957d58d289bd2c8aeebd729017
jash200115 <83970478+jash200115@users.noreply.github.com>
2022-07-01 14:19:14 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,33 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
+const validator = require('validator');
 
 const UserSchema = new Schema({
-    // email: {
-    //     type: String,
-    //     required: true,
-    //     unique: true
-    // }
-      name: {
+    email: {
         type: String,
-        required: true
-      },
-      email: {
-        type: String,
-        required: true
-      },
-      password: {
-        type: String,
-        required: true
-      },
-      verified: {
-        type: Boolean,
-        default: false
-      },
-      resetLink: {
-        type: String,
-        default: ''
+        required: true,
+        unique: true,
+        validate(value){
+          if(!validator.isEmail(value)){
+            throw new Error("Email is Invalid, Try Again");
+          }
+        }
+    },
+    contact: {
+      type: Number,
+      validate: {
+          validator: function(v) {
+              return /^[7-9][0-9]{9}$/.test(v);
+          },
+          message: '{VALUE} is not a valid 10 digit number!'
       }
+  }
 });
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User',UserSchema);
